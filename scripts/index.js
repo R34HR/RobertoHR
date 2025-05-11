da6d33cc738424d36853c7dbeb251ba448f48ce1
Drox <robertoahr34@gmail.com>
2025-05-11 13:55:27 -0700

    Created a second styling sheet to try and reduce redundent or unnecessary css.

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-const navbarCollapse = document.querySelector(".navbar-collapse");
-const navLinks = document.querySelectorAll(".nav-link");
-
-navLinks.forEach(link => {
-    link.addEventListener("click", function () {
-    if (navbarCollapse.classList.contains("show")) {
-        new bootstrap.Collapse(navbarCollapse, { toggle: true });
-    }
-    });
-});
-});
-// Adds/removes a class animation name when a heading comes into view.
-const observer = new IntersectionObserver(entries, id => {
-    entries.forEach(entry => {
-      // const heading = entry.target.querySelector('#welcome_head');
-      const heading = id;
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+  const navLinks = document.querySelectorAll(".nav-link");
   
-      if (entry.isIntersecting) {
-        heading.classList.add('lineUp');
-        return; // if we added the class, exit the function
+  navLinks.forEach(link => {
+      link.addEventListener("click", function () {
+      if (navbarCollapse.classList.contains("show")) {
+          new bootstrap.Collapse(navbarCollapse, { toggle: true });
       }
-  
-      // We're not intersecting, so remove the class!
-      heading.classList.remove('lineUp');
-
-      // window.addEventListener('scroll', function(){
-      //   if(entry.isIntersecting){
-      //     heading.classList
-      //   }
-      // });
-    });
+      });
   });
+  });
+  // Adds/removes a class animation name when a heading comes into view.
+  const observer = new IntersectionObserver(entries, id => {
+      entries.forEach(entry => {
+        // const heading = entry.target.querySelector('#welcome_head');
+        const heading = id;
+    
+        if (entry.isIntersecting) {
+          heading.classList.add('lineUp');
+          return; // if we added the class, exit the function
+        }
+    
+        // We're not intersecting, so remove the class!
+        heading.classList.remove('lineUp');
   
-  // observer.observe(document.querySelector('#welcome_title'));
+      });
+    });
+    
   observer.observe(document.querySelector('#welcome_title'), document.querySelector('#welcome_head'));
